c05bb0ffcebbecd95d9d5ddc3c86eca7718ba5f6
Vichisorn <james31366@gmail.com>
2021-09-07 20:28:52 +0700

    add test cases for Inventory and coffee maker

--- src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
+++ src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -39,20 +39,48 @@ import java.io.PrintStream;
  */
 public class CoffeeMakerTest {
 
+    //Used to capture the System.out
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
     /**
      * The object under test.
      */
     private CoffeeMaker coffeeMaker;
-
     // Sample recipes to use in testing.
     private Recipe recipe1;
     private Recipe recipe2;
     private Recipe recipe3;
     private Recipe recipe4;
 
-    //Used to capture the System.out
-    private final PrintStream standardOut = System.out;
-    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private static Recipe createRecipe(String name, String amtChocolate, String amtCoffee, String amtMilk, String amtSugar, String price) throws RecipeException {
+        Recipe recipe = new Recipe();
+        recipe.setName(name);
+        recipe.setAmtChocolate(amtChocolate);
+        recipe.setAmtCoffee(amtCoffee);
+        recipe.setAmtMilk(amtMilk);
+        recipe.setAmtSugar(amtSugar);
+        recipe.setPrice(price);
+
+        return recipe;
+    }
+
+    private static StringBuffer createStringBuffer(int amtCoffee, int amtMilk, int amtSugar, int amtChocolate) {
+        StringBuffer buf = new StringBuffer();
+        buf.append("Coffee: ");
+        buf.append(amtCoffee);
+        buf.append("\n");
+        buf.append("Milk: ");
+        buf.append(amtMilk);
+        buf.append("\n");
+        buf.append("Sugar: ");
+        buf.append(amtSugar);
+        buf.append("\n");
+        buf.append("Chocolate: ");
+        buf.append(amtChocolate);
+        buf.append("\n");
+
+        return buf;
+    }
 
     /**
      * Initializes some recipes to test with and the {@link CoffeeMaker}
@@ -68,41 +96,18 @@ public class CoffeeMakerTest {
         System.setOut(new PrintStream(outputStreamCaptor));
 
         //Set up for r1
-        recipe1 = new Recipe();
-        recipe1.setName("Coffee");
-        recipe1.setAmtChocolate("0");
-        recipe1.setAmtCoffee("3");
-        recipe1.setAmtMilk("1");
-        recipe1.setAmtSugar("1");
-        recipe1.setPrice("50");
+        recipe1 = createRecipe("Coffee", "0", "3", "1", "1", "50");
 
         //Set up for r2
-        recipe2 = new Recipe();
-        recipe2.setName("Mocha");
-        recipe2.setAmtChocolate("20");
-        recipe2.setAmtCoffee("3");
-        recipe2.setAmtMilk("1");
-        recipe2.setAmtSugar("1");
-        recipe2.setPrice("75");
+        recipe2 = createRecipe("Mocha", "20", "3", "1", "1", "75");
 
         //Set up for r3
-        recipe3 = new Recipe();
-        recipe3.setName("Latte");
-        recipe3.setAmtChocolate("0");
-        recipe3.setAmtCoffee("3");
-        recipe3.setAmtMilk("3");
-        recipe3.setAmtSugar("1");
-        recipe3.setPrice("100");
+        recipe3 = createRecipe("Latte", "0", "3", "3", "1", "100");
 
         //Set up for r4
-        recipe4 = new Recipe();
-        recipe4.setName("Hot Chocolate");
-        recipe4.setAmtChocolate("4");
-        recipe4.setAmtCoffee("0");
-        recipe4.setAmtMilk("1");
-        recipe4.setAmtSugar("1");
-        recipe4.setPrice("65");
+        recipe4 = createRecipe("Hot Chocolate", "4", "0", "1", "1", "65");
     }
+
 
     /**
      * The Recipe Book cannot add more than three Recipe into List if add more than three it should fail.
@@ -129,88 +134,83 @@ public class CoffeeMakerTest {
      */
     @Test
     public void testAddBlankNameRecipe() throws RecipeException {
-        Recipe tmpRecipe = new Recipe();
-        tmpRecipe.setName("");
-        tmpRecipe.setAmtChocolate("0");
-        tmpRecipe.setAmtCoffee("3");
-        tmpRecipe.setAmtMilk("1");
-        tmpRecipe.setAmtSugar("1");
-        tmpRecipe.setPrice("50");
+        Recipe tmpRecipe = createRecipe("", "0", "3", "1", "1", "50");
         assertFalse("Shouldn't add the black name into the recipe book", coffeeMaker.addRecipe(tmpRecipe));
     }
 
+    /**
+     * Check delete recipe function that work correctly or not.
+     *
+     * @throws RecipeException Exception for Recipe Class
+     */
     @Test
     public void testDeleteRecipe() throws RecipeException {
         CoffeeMaker tmpCoffeeMaker = new CoffeeMaker();
-        Recipe tmpRecipe = new Recipe();
-        tmpRecipe.setName("Latte");
-        tmpRecipe.setAmtChocolate("0");
-        tmpRecipe.setAmtCoffee("3");
-        tmpRecipe.setAmtMilk("1");
-        tmpRecipe.setAmtSugar("1");
-        tmpRecipe.setPrice("50");
+        Recipe tmpRecipe = createRecipe("Latte", "0", "3", "1", "1", "50");
         assertTrue(tmpCoffeeMaker.addRecipe(tmpRecipe));
         assertEquals(tmpCoffeeMaker.deleteRecipe(0), "Latte");
     }
 
+    /**
+     * Delete the null recipe.
+     */
     @Test
-    public void testDeleteNullRecipe() throws RecipeException {
+    public void testDeleteNullRecipe() {
         assertNull(coffeeMaker.deleteRecipe(0));
     }
 
+    /**
+     * Delete the recipe that input value of recipeToDelete is negative number.
+     *
+     * @throws RecipeException Exception for Recipe Class
+     */
     @Test
     public void testDeleteNegativeArrayRecipe() throws RecipeException {
         assertThrows(RecipeException.class, () -> coffeeMaker.deleteRecipe(-1));
     }
 
+    /**
+     * Delete the recipe that input value of recipeToDelete is not in list.
+     */
     @Test
-    public void testDeleteRecipeNotInList() throws RecipeException {
+    public void testDeleteRecipeNotInList() {
         assertThrows(RecipeException.class, () -> coffeeMaker.deleteRecipe(11));
     }
 
+    /**
+     * Check edit recipe function with normal way.
+     *
+     * @throws RecipeException Exception for Recipe class.
+     */
     @Test
     public void testEditRecipe() throws RecipeException {
         CoffeeMaker tmpCoffeeMaker = new CoffeeMaker();
-        Recipe tmpRecipe = new Recipe();
-        tmpRecipe.setName("Latte");
-        tmpRecipe.setAmtChocolate("0");
-        tmpRecipe.setAmtCoffee("3");
-        tmpRecipe.setAmtMilk("1");
-        tmpRecipe.setAmtSugar("1");
-        tmpRecipe.setPrice("50");
-
-        Recipe newRecipe = new Recipe();
-        newRecipe.setName("Latte");
-        newRecipe.setAmtChocolate("5");
-        newRecipe.setAmtCoffee("3");
-        newRecipe.setAmtMilk("1");
-        newRecipe.setAmtSugar("1");
-        newRecipe.setPrice("75");
+        Recipe tmpRecipe = createRecipe("Latte", "0", "3", "1", "1", "50");
         assertTrue(tmpCoffeeMaker.addRecipe(tmpRecipe));
+
+        Recipe newRecipe = createRecipe("Latte", "5", "3", "1", "1", "75");
         assertEquals(tmpCoffeeMaker.editRecipe(0, newRecipe), "Latte");
     }
 
+    /**
+     * Edit the recipe that input value of recipeToEdit as NegativeNumber.
+     *
+     * @throws RecipeException Exception for Recipe class.
+     */
     @Test
     public void testEditNegativeArrayRecipe() throws RecipeException {
-        Recipe newRecipe = new Recipe();
-        newRecipe.setName("Latte");
-        newRecipe.setAmtChocolate("5");
-        newRecipe.setAmtCoffee("3");
-        newRecipe.setAmtMilk("1");
-        newRecipe.setAmtSugar("1");
-        newRecipe.setPrice("75");
+        Recipe newRecipe = createRecipe("Latte", "5", "3", "1", "1", "75");
         assertThrows(RecipeException.class, () -> coffeeMaker.editRecipe(-1, newRecipe));
     }
 
+    /**
+     * Edit the recipe that input value of recipeToEdit is not in List.
+     *
+     * @throws RecipeException Exception for Recipe class.
+     */
     @Test
     public void testEditRecipeNotInList() throws RecipeException {
-        Recipe newRecipe = new Recipe();
-        newRecipe.setName("Latte");
-        newRecipe.setAmtChocolate("5");
-        newRecipe.setAmtCoffee("3");
-        newRecipe.setAmtMilk("1");
-        newRecipe.setAmtSugar("1");
-        newRecipe.setPrice("75");
+        Recipe newRecipe = createRecipe("Latte", "5", "3", "1", "1", "75");
         assertThrows(RecipeException.class, () -> coffeeMaker.editRecipe(11, newRecipe));
     }
 
@@ -223,27 +223,41 @@ public class CoffeeMakerTest {
      *                            to a positive integer.
      */
     @Test
-    public void testAddInventory() throws InventoryException {
-        coffeeMaker.addInventory("4", "7", "0", "9");
+    public void testAddPositiveIngredientToInventory() throws InventoryException {
+        coffeeMaker.addInventory("1", "0", "0", "0");
+        assertEquals("Positive coffee amount should be able to add to Inventory ", "Coffee: 16\nMilk: 15\nSugar: 15\nChocolate: 15\n", coffeeMaker.checkInventory());
+
+        coffeeMaker.addInventory("0", "1", "0", "0");
+        assertEquals("Positive milk amount should be able to add to Inventory ", "Coffee: 16\nMilk: 16\nSugar: 15\nChocolate: 15\n", coffeeMaker.checkInventory());
+
+        coffeeMaker.addInventory("0", "0", "1", "0");
+        assertEquals("Positive sugar amount should be able to add to Inventory ", "Coffee: 16\nMilk: 16\nSugar: 16\nChocolate: 15\n", coffeeMaker.checkInventory());
+
+        coffeeMaker.addInventory("0", "0", "0", "1");
+        assertEquals("Positive chocolate amount should be able to add to Inventory ", "Coffee: 16\nMilk: 16\nSugar: 16\nChocolate: 16\n", coffeeMaker.checkInventory());
     }
 
+    /**
+     * Add the Negative Coffee amount into add Inventory function.
+     */
     @Test
     public void testAddNegativeIngredientToInventory() {
+        StringBuffer buf = createStringBuffer(15, 15, 15, 15);
         assertThrows("Check amtCoffee to be Negative", InventoryException.class, () -> {
             coffeeMaker.addInventory("-1", "7", "0", "9");
-            coffeeMaker.checkInventory();
+            assertEquals("Negative Coffee amount couldn't add to Inventory", buf.toString(), coffeeMaker.checkInventory());
         });
         assertThrows("Check amtMilk to be Negative", InventoryException.class, () -> {
             coffeeMaker.addInventory("4", "-1", "0", "9");
-            coffeeMaker.checkInventory();
-        });
-        assertThrows("Check amtSugar to be Negative", InventoryException.class, () -> {
-            coffeeMaker.addInventory("4", "7", "-1", "9");
-            coffeeMaker.checkInventory();
+            assertEquals("Negative Milk amount couldn't add to Inventory", buf.toString(), coffeeMaker.checkInventory());
         });
         assertThrows("Check amtChocolate to be Negative", InventoryException.class, () -> {
             coffeeMaker.addInventory("4", "7", "0", "-1");
-            coffeeMaker.checkInventory();
+            assertEquals("Negative Sugar amount couldn't add to Inventory", buf.toString(), coffeeMaker.checkInventory());
+        });
+        assertThrows("Check amtSugar to be Negative", InventoryException.class, () -> {
+            coffeeMaker.addInventory("4", "7", "-1", "9");
+            assertEquals("Negative Chocolate amount couldn't add to Inventory", buf.toString(), coffeeMaker.checkInventory());
         });
     }
 
@@ -255,40 +269,29 @@ public class CoffeeMakerTest {
      */
     @Test
     public void testAddInventoryException() {
+        StringBuffer buf = createStringBuffer(15, 15, 15, 15);
         assertThrows("Input String in amtCoffee", InventoryException.class, () -> {
-            coffeeMaker.addInventory("asdf", "-1", "0", "3");
-            coffeeMaker.checkInventory();
+            coffeeMaker.addInventory("asdf", "7", "0", "3");
+            assertEquals("Coffee amount as String couldn't add to Inventory", buf.toString(), coffeeMaker.checkInventory());
         });
         assertThrows("Input String in amtMilk", InventoryException.class, () -> {
             coffeeMaker.addInventory("4", "asdf", "0", "3");
-            coffeeMaker.checkInventory();
+            assertEquals("Milk amount as String couldn't add to Inventory", buf.toString(), coffeeMaker.checkInventory());
         });
         assertThrows("Input String in amtSugar", InventoryException.class, () -> {
-            coffeeMaker.addInventory("4", "-1", "asdf", "3");
-            coffeeMaker.checkInventory();
+            coffeeMaker.addInventory("4", "7", "asdf", "3");
+            assertEquals("Sugar amount as String couldn't add to Inventory", buf.toString(), coffeeMaker.checkInventory());
         });
         assertThrows("Input String in amtChocolate", InventoryException.class, () -> {
-            coffeeMaker.addInventory("4", "-1", "0", "asdf");
-            coffeeMaker.checkInventory();
+            coffeeMaker.addInventory("4", "7", "0", "asdf");
+            assertEquals("Chocolate amount as String couldn't add to Inventory", buf.toString(), coffeeMaker.checkInventory());
         });
     }
 
     @Test
-    public void testCheckInventory() throws InventoryException {
-        StringBuffer buf = new StringBuffer();
-        buf.append("Coffee: ");
-        buf.append(15);
-        buf.append("\n");
-        buf.append("Milk: ");
-        buf.append(15);
-        buf.append("\n");
-        buf.append("Sugar: ");
-        buf.append(15);
-        buf.append("\n");
-        buf.append("Chocolate: ");
-        buf.append(15);
-        buf.append("\n");
-        assertEquals(coffeeMaker.checkInventory(), buf.toString());
+    public void testCheckInventoryToInventory() {
+        StringBuffer buf = createStringBuffer(15, 15, 15, 15);
+        assertEquals(buf.toString(), coffeeMaker.checkInventory());
     }
 
     /**
@@ -304,24 +307,33 @@ public class CoffeeMakerTest {
     }
 
     @Test
-    public void testPurchaseBeverageWithInventory() throws InventoryException {
+    public void testPurchaseBeverageWithInventory() {
         coffeeMaker.addRecipe(recipe1);
         assertEquals(25, coffeeMaker.makeCoffee(0, 75));
 
-        StringBuffer buf = new StringBuffer();
-        buf.append("Coffee: ");
-        buf.append(12);
-        buf.append("\n");
-        buf.append("Milk: ");
-        buf.append(14);
-        buf.append("\n");
-        buf.append("Sugar: ");
-        buf.append(14);
-        buf.append("\n");
-        buf.append("Chocolate: ");
-        buf.append(15);
-        buf.append("\n");
-
-        assertEquals(coffeeMaker.checkInventory(), buf.toString());
+        StringBuffer buf = createStringBuffer(12, 14, 14, 15);
+        assertEquals(buf.toString(), coffeeMaker.checkInventory());
     }
+
+    @Test
+    public void testPurchaseBeverageWithNullRecipe() {
+        assertThrows(InventoryException.class, () -> {
+            coffeeMaker.makeCoffee(3, 75);
+        });
+    }
+
+    @Test
+    public void testPurchaseBeverageWithNotEnoughIngredient() throws RecipeException {
+        Recipe blackCoffee = createRecipe("Black Coffee", "16", "16", "16", "16", "50");
+        coffeeMaker.addRecipe(blackCoffee);
+        assertEquals(100, coffeeMaker.makeCoffee(0, 100));
+    }
+
+    @Test
+    public void testPurchaseBeverageWithNotEnoughMoney() {
+        coffeeMaker.addRecipe(recipe1);
+        assertEquals(1, coffeeMaker.makeCoffee(0, 1));
+    }
+
+
 }
